f269374e7f97664e604b578e773f371b541274e2
Srikripa <srikripa.sm@gmail.com>
2020-10-01 14:53:23 +0530

    first commit X

---
+++ src/test/java/practical1/AppTest.java
@@ -0,0 +1,36 @@
+package practical1;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 * @return
+	 */
+	@Test
+	public void AppTest1(String testName) {
+		// System.setProperty("webdriver.chrome.driver",
+		// System.getProperty("user.dir") +
+		// "//src//main//java//practical//chromedriver.exe");
+		// WebDriver driver = new ChromeDriver();
+		// driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+		// driver.close();
+
+		// 3 6 9 12 15 18
+		List<String> str = Arrays.asList("adam", "don", "major", "kara", "susha", "allam");
+		Boolean b = str.stream().anyMatch(s -> s.contains("con"));
+		System.out.println(b);
+		// adding a comment
+		// adding anther
+	}
+
+}
